2349e7fe99ee19fa96efdb24862f9bdf477d2791
sophia <sophiendalo@gmail.com>
2024-03-18 23:48:57 +0300

    1.Update Navbar.jsx; change work list to projects

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
                   to="/work" // Define the route path to your Work component
                   className="block py-2 pl-3 pr-4 text-white rounded md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark:text-white md:dark:hover-text-blue-500 dark:hover-bg-gray-700 dark:hover-text-white md:dark:hover-bg-transparent"
                 >
-                  Work
+                  projects
                 </Link>
               </li>
               <li className="mb-4">
